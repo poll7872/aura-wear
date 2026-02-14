ec7c4770592f73a8722e056da7f5085407db8482
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-02-13 20:07:12 -0500

    feat: add CreateProductDto

---
+++ backend/src/products/dto/create-product.dto.ts
@@ -0,0 +1,19 @@
+import { IsInt, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty({ message: 'El nombre del producto no puede ir vacio' })
+  @IsString({ message: 'Nombre no valido' })
+  name: string;
+
+  @IsNotEmpty({ message: 'El precio del producto no puede ir vacio' })
+  @IsNumber({ maxDecimalPlaces: 2 }, { message: 'Precio no valido' })
+  price: number;
+
+  @IsNotEmpty({ message: 'La cantidad no puede ir vacia' })
+  @IsInt({ message: 'Cantidad no valida' })
+  inventory: number;
+
+  @IsNotEmpty({ message: 'La categoria es obligatoria' })
+  @IsInt({ message: 'Categoria no valida' })
+  categoryId: number;
+}
