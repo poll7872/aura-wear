61e935a46c64431a495140d3e76feaff70d09a29
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-02-13 20:05:04 -0500

    fix: add validation message to CreateCategoryDto

--- backend/src/categories/dto/create-category.dto.ts
+++ backend/src/categories/dto/create-category.dto.ts
@@ -2,6 +2,6 @@ import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCategoryDto {
   @IsNotEmpty({ message: 'El nombre de la categoria no puede estar vacio' })
-  @IsString()
+  @IsString({ message: 'Valor no valido' })
   name: string;
 }
