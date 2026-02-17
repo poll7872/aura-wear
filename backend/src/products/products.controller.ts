78861a41ea512391dd6d19e74ecafbdc259b3b74
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-02-16 19:09:16 -0500

    chore: update backend/src/products/products.controller.ts

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -3,15 +3,16 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
   Query,
+  Put,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { GetProductQueryDto } from './dto/get-product.dto';
+import { IdValidationPipe } from 'src/common/pipes/id-validation/id-validation.pipe';
 
 @Controller('products')
 export class ProductsController {
@@ -24,21 +25,24 @@ export class ProductsController {
 
   @Get()
   findAll(@Query() query: GetProductQueryDto) {
-    return this.productsService.findAll(query.category_id);
+    return this.productsService.findAll(query);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', IdValidationPipe) id: string) {
     return this.productsService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  @Put(':id')
+  update(
+    @Param('id', IdValidationPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
     return this.productsService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', IdValidationPipe) id: string) {
     return this.productsService.remove(+id);
   }
 }
