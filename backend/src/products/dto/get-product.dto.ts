28d8ff965ee3d06e7091117ef12487745d0916cb
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-02-16 19:09:16 -0500

    chore: update backend/src/products/dto/get-product.dto.ts

--- backend/src/products/dto/get-product.dto.ts
+++ backend/src/products/dto/get-product.dto.ts
@@ -1,9 +1,20 @@
 import { Type } from 'class-transformer';
-import { IsInt, IsOptional } from 'class-validator';
+import { IsInt, IsOptional, Min } from 'class-validator';
 
 export class GetProductQueryDto {
   @IsOptional()
   @Type(() => Number)
   @IsInt({ message: 'La categoria debe ser un numero' })
-  category_id?: number;
+  category_id: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt({ message: 'La cantidad debe ser un numero' })
+  @Min(1, { message: 'La cantidad debe ser mayor o igual a 1' })
+  take: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt({ message: 'La cantidad debe ser un numero' })
+  skip: number;
 }
