6d236107544391057eb6d21747665a6e50437105
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-02-25 20:53:23 -0500

    feat(schemas): incorpora esquemas y tipo de transacciones

--- frontend/src/schemas.ts
+++ frontend/src/schemas.ts
@@ -63,7 +63,26 @@ export const ErrorResponseSchema = z.object({
   statusCode: z.number(),
 });
 
+export const ContentsSchema = z.object({
+  id: z.number(),
+  quantity: z.number(),
+  price: z.string(),
+  product: ProductSchema,
+});
+
+export const TransactionResponseSchema = z.object({
+  id: z.number(),
+  total: z.string(),
+  transactionDate: z.string(),
+  discount: z.string().nullable(),
+  coupon: z.string().nullable(),
+  contents: z.array(ContentsSchema),
+});
+
+export const TransactionsResponseSchema = z.array(TransactionResponseSchema);
+
 export type Product = z.infer<typeof ProductSchema>;
 export type ShoppingCart = z.infer<typeof ShoppingCartSchema>;
 export type CartItem = z.infer<typeof ShoppingCartContentSchema>;
 export type Coupon = z.infer<typeof CouponResponseSchema>;
+export type Transaction = z.infer<typeof TransactionResponseSchema>;
