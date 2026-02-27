ab2c5ebca6b9f0c79ecaaa73b9f8b7251989b0e6
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-02-26 21:09:43 -0500

    feat(utils): anadir validacion de numero de pagina

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -5,3 +5,22 @@ export const formatCurrency = (value: number) => {
     minimumFractionDigits: 0,
   }).format(value);
 };
+
+export function isValidPage(value: number) {
+  if (value == null) {
+    return false;
+  }
+
+  if (typeof value !== "number" && isNaN(value)) {
+    return false;
+  }
+  if (value <= 0) {
+    return false;
+  }
+
+  if (!Number.isInteger(value)) {
+    return false;
+  }
+
+  return true;
+}
