dfd61f33d3865a6945d0da928779539211df42b4
Edward Ojeda <edward.ojeda.es@gmail.com>
2026-02-26 21:09:43 -0500

    refactor(products): devolver objeto de confirmacion al eliminar producto

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -97,6 +97,8 @@ export class ProductsService {
   async remove(id: number) {
     const product = await this.findOne(id);
     await this.productRepository.remove(product);
-    return 'Producto eliminado';
+    return {
+      message: 'Producto Eliminado con Exito',
+    };
   }
 }
